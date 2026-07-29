1561b60b218a99454436fd331cb9ecd6ebaebab3
xenokarma <andikapurnama1803@gmail.com>
2026-07-29 22:53:03 +0700

    day 1 sudah belajar tentang tipe data, var, rune dan next belajar function

--- app.go
+++ app.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+// cara nyimpen varabel didalam golang ada 2 cara
+// 1 var
+// var nama string = "Dimas"
+// var usia uint = 27
+// var isCompleted bool = true
+
 func main() {
-	fmt.Println("Hello Pace")
+	// short declaration :=
+	nama := "Budi"
+	usia := 30
+	isCompleted := false
+	tinggiBadan := 169.6
+	// rune
+	// merupakan tipe data yang digunakan untuk menyimpan karakter
+	emoji := '😎'
+
+
+	fmt.Println("Nama saya : ", nama)
+	fmt.Println("Usia saya : ", usia)
+	fmt.Println("Apakah tugas selesai? : ", isCompleted)
+	fmt.Println("Tinggi badan saya : ", tinggiBadan)
+	fmt.Println("Emoji saya : ", emoji)
 }

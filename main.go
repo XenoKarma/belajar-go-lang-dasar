e379ded13ea74a2108acd72802ed83bcb58e7800
xenokarma <andikapurnama1803@gmail.com>
2026-08-01 18:53:24 +0700

    conditional go lang

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
-func data() string {
-	nama := "Andika"
-	umur := 20
-	tinggi := 170.5
-	lulus := true
-
-	return fmt.Sprintf("Halo, nama saya %s, umur saya %d tahun, dengan tinggi badan %.1f cm, dan saya %t dari covid", nama, umur, tinggi, lulus)
-}
-
 func main() {
-	fmt.Println(data())
+	var color string
+	fmt.Print("Masukkan Warna Lampu lalu lintas: ")
+	fmt.Scanln(&color)
+
+	switch color {
+	case "red":
+		fmt.Println("Lampu Merah: Berhenti")
+	case "yellow":
+		fmt.Println("Lampu Kuning: Hati-hati")
+	case "green":
+		fmt.Println("Lampu Hijau: Jalan")
+	default:
+		fmt.Println("Warna lampu tidak valid")
+	}
+	
 }

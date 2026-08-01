b1902ab36f95827a22ecc7e58a48d1fac55b3cef
xenokarma <andikapurnama1803@gmail.com>
2026-08-01 19:10:59 +0700

    v2 string package golang

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
-package main
+package main //package utama. Program Go selalu dimulai dengan package main. Package ini digunakan untuk menandai bahwa file ini adalah file utama yang akan dijalankan.
 
 import (
-	"fmt"
-	"strings"
+	"fmt"     //package untuk input dan output
+	"strings" //package untuk manipulasi string
 )
 
 func main() {
+	//deklarasi variabel
 	nama := "andika"
+
+	//manipulasi string
 	animals := "kucing, anjing, burung"
+
+	//memisahkan string menjadi slice
 	filter := strings.Split(animals, ",")
+
+	//menghapus spasi di awal dan akhir string
 	cihuy := "          hello world           "
 	filterCihuy := strings.TrimSpace(cihuy)
 
